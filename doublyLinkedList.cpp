cc11ff01dfca53ed659ef3ba75e3544d8614dae4
Fannandya <fannandya.sutan.ft24@mail.umy.ac.id>
2025-05-15 10:58:42 +0700

    memasukkan node diantara current dan current next

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -70,6 +70,8 @@ public:
             return;
         }
 
-        s
+        // step 9: insert between current and current next
+        newNode->next = current->next; // step 9a
+        newNode->prev = current;       // step 9b
     }
 };
