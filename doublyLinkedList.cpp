4fdf3bcc048cc5f9f18dd6dab94b3631e7aee9ff
Fannandya <fannandya.sutan.ft24@mail.umy.ac.id>
2025-05-15 10:56:21 +0700

    mencari posisi untuk menambahkan node baru

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -56,5 +56,20 @@ public:
             START = newNode;
             return;
         }
+        // insert in between node
+        // step 8: locate potition for insertion
+        node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll number not allowed" << endl;
+            return;
+        }
+
+        s
     }
 };
