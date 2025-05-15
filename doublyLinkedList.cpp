ce5ba64329e35f269cfb4b940000a4dd348a8faa
Fannandya <fannandya.sutan.ft24@mail.umy.ac.id>
2025-05-15 11:51:32 +0700

    membuat pilihan menu

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -225,6 +225,28 @@ public:
             cout << "enter your choice: ";
 
             cin >> choice;
+
+            switch (choice)
+            {
+            case '1':
+                list.addNode();
+                break;
+            case '2':
+                list.hapus();
+                break;
+            case '3':
+                list.traverse();
+                break;
+            case '4':
+                list.revtraverse();
+                break;
+            case '5':
+                list.searchData();
+                break;
+            case '6':
+                return 0;
+            default:
+                cout << "invalid option\n";
+            }
         }
-        }
-};
+    };
